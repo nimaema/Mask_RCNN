f8af85c9c5f7beb03da0c519fd0fcab5f7834ea3
Nima Emami <nimaemami31@gmail.com>
2021-03-22 12:21:05 +0100

    initializing

---
+++ samples/fiber/fiber.py
@@ -0,0 +1,310 @@
+if __name__ == '__main__':
+    import matplotlib
+    # Agg backend runs without a display
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+import os
+import sys
+import json
+import datetime
+import numpy as np
+from numpy.lib.type_check import imag
+from skimage import io
+from imgaug import augmenters as iaa
+from skimage.color import rgba2rgb
+from skimage.color import rgb2gray
+import tensorflow as tf
+import random
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+from mrcnn import model as modellib
+from mrcnn import visualize
+# COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# import fiber
+# Path to trained weights file
+# COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Results directory
+# Save submission files here
+RESULTS_DIR = os.path.join(ROOT_DIR, "results/fiber")
+
+# The dataset doesn't have a standard train/val split, so I picked
+# a variety of images to surve as a validation set.
+VAL_IMAGE_IDS = [str(i) for i in range(1,7)]
+
+
+
+
+
+# print(VAL_IMAGE_IDS)
+############################################################
+#  Configurations
+############################################################
+
+class FiberConfig(Config):
+
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "fiber"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 24
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + fiber
+
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.90
+    
+    STEPS_PER_EPOCH = (8673 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
+    VALIDATION_STEPS = max(1, len(VAL_IMAGE_IDS) // IMAGES_PER_GPU)
+
+
+
+
+class FiberInferenceConfig(FiberConfig):
+    # Set batch size to 1 to run one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Don't resize imager for inferencing
+    IMAGE_RESIZE_MODE = "pad64"
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.7
+
+
+############################################################
+#  Dataset
+############################################################
+
+class FiberDataset(utils.Dataset):
+
+    def load_fiber(self, dataset_dir, subset):
+        """Load a subset of the nuclei dataset.
+
+        dataset_dir: Root directory of the dataset
+        subset: Subset to load. Either the name of the sub-directory,
+                such as stage1_train, stage1_test, ...etc. or, one of18
+                * train: stage1_train excluding validation images
+                * val: validation images from VAL_IMAGE_IDS
+        """
+        # Add classes. We have one class.
+        # Naming the dataset nucleus, and the class nucleus
+        self.add_class("fiber", 1, "fiber")
+
+        # Which subset?
+        # "val": use hard-coded list above
+        # "train": use data from stage1_train minus the hard-coded list above
+        # else: use the data from the specified sub-directory
+        subset_dir = "5000"
+        dataset_dir = os.path.join(dataset_dir, subset_dir)
+        if subset == "val":
+            image_ids = VAL_IMAGE_IDS
+        else:
+            # Get image ids from directory names
+            image_ids = next(os.walk(dataset_dir))[1]
+
+            if subset == "train":
+                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
+
+
+
+        for image_id in image_ids:
+
+            self.add_image(
+                "fiber",
+                image_id=image_id,
+                path=os.path.join(dataset_dir, image_id, "image/gt_{}.png".format(image_id)))
+
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        dataset_dir = os.getcwd()
+        info = self.image_info[image_id]
+
+        # Get mask directory from image path
+        mask_dir = os.path.join(os.path.dirname(
+            os.path.dirname(info['path'])), "mask")
+
+        # Read mask files from .png image
+        mask = []
+        for f in next(os.walk(mask_dir))[2]:
+            if f.endswith(".png"):
+                m = io.imread(os.path.join(
+                    mask_dir, f)).astype(np.bool)
+                m = rgb2gray(rgba2rgb(m))#.astype(np.bool)
+                mask.append(m)
+        mask = np.stack(mask, axis=-1)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID, we return an array of ones
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "fiber":
+            return info["id"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+DATASET_DIR = os.getcwd()
+#
+# # Dataset directory
+# # DATASET_DIR = os.path.join(ROOT_DIR, "datasets/nucleus")
+#
+# Inference Configuration
+config = FiberInferenceConfig()
+config.display()
+
+dataset_train = FiberDataset()
+dataset_train.load_fiber(DATASET_DIR, subset="train")
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = FiberDataset()
+dataset_val.load_fiber(DATASET_DIR, subset="val")
+dataset_val.prepare()
+
+model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=ROOT_DIR)
+# model.load_weights(COCO_WEIGHTS_PATH, by_name=True, exclude=[
+#             "mrcnn_class_logits", "mrcnn_bbox_fc",
+#             "mrcnn_bbox", "mrcnn_mask"])
+# tf.test.gpu_device_name()
+print("Train network heads")
+model.train(dataset_train, dataset_val,
+            learning_rate=config.LEARNING_RATE/3,
+            epochs=250,
+            layers='all')
+
+
+def rle_encode(mask):
+    """Encodes a mask in Run Length Encoding (RLE).
+    Returns a string of space-separated values.
+    """
+    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
+    # Flatten it column wise
+    m = mask.T.flatten()
+    # Compute gradient. Equals 1 or -1 at transition points
+    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
+    # 1-based indicies of transition points (where gradient != 0)
+    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
+    # Convert second index in each pair to lenth
+    rle[:, 1] = rle[:, 1] - rle[:, 0]
+    return " ".join(map(str, rle.flatten()))
+
+
+def rle_decode(rle, shape):
+    """Decodes an RLE encoded list of space separated
+    numbers and returns a binary mask."""
+    rle = list(map(int, rle.split()))
+    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
+    rle[:, 1] += rle[:, 0]
+    rle -= 1
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for s, e in rle:
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
+        mask[s:e] = 1
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+
+def mask_to_rle(image_id, mask, scores):
+    "Encodes instance masks to submission format."
+    assert mask.ndim == 3, "Mask must be [H, W, count]"
+    # If mask is empty, return line with image ID only
+    if mask.shape[-1] == 0:
+        return "{},".format(image_id)
+    # Remove mask overlaps
+    # Multiply each instance mask by its score order
+    # then take the maximum across the last dimension
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
+    # Loop over instance masks
+    lines = []
+    for o in order:
+        m = np.where(mask == o, 1, 0)
+        # Skip if empty
+        if m.sum() == 0.0:
+            continue
+        rle = rle_encode(m)
+        lines.append("{}, {}".format(image_id, rle))
+    return "\n".join(lines)
+
+
+############################################################
+#  Detection
+############################################################
+
+def detect(model, dataset_dir, subset):
+    """Run detection on images in the given directory."""
+    print("Running on {}".format(dataset_dir))
+
+    # Create directory
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+    os.makedirs(submit_dir)
+
+    # Read dataset
+    dataset = FiberDataset()
+    dataset.load_fiber(dataset_dir, subset)
+    dataset.prepare()
+    # Load over images
+    submission = []
+    for image_id in dataset.image_ids:
+        # Load image and run detection
+        image = dataset.load_image(image_id)
+        # Detect objects
+        r = model.detect([image], verbose=0)[0]
+        # Encode image to RLE. Returns a string of multiple lines
+        source_id = dataset.image_info[image_id]["id"]
+        rle = mask_to_rle(source_id, r["masks"], r["scores"])
+        submission.append(rle)
+        # Save image with masks
+        visualize.display_instances(
+            image, r['rois'], r['masks'], r['class_ids'],
+            dataset.class_names,
+            show_bbox=False, show_mask=True,
+            title="Predictions")
+        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+
+    # Save to csv file
+    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
+    file_path = os.path.join(submit_dir, "submit.csv")
+    with open(file_path, "w") as f:
+        f.write(submission)
+    print("Saved to ", submit_dir)
+
+config = FiberInferenceConfig()
+model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=ROOT_DIR)
+weights_path = model.find_last()
+model.load_weights(weights_path, by_name=True)
+
+subset = "val"
+
+detect(model, DATASET_DIR, subset)
